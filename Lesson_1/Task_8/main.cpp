ee7f57648c468806d24e1ee014ede5d1439f8167
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2020-12-06 22:50:59 +0200

    update lesson 1 with .gitignore

--- Lesson_1/Task_8/main.cpp
+++ Lesson_1/Task_8/main.cpp
@@ -5,17 +5,21 @@ using namespace std;
 int main()
 {
     int a = 0;
+    int b = 0;
+    int c = 0;
     cout << "Enter size of the christmas tree:";
     cin >> a;
-    for (int b = 0; b < a; b++)
+    for (b = 0; b < a; b++)
     {
-        for (int c = 1; c < a - b; c++)
+        for (c = 1; c < a - b; c++)
             cout << " ";
-        for (int c = a - 2*b; c <= a; c++)
+        for (c = 0; c < b * 2 + 1; c++)
             cout << "*";
         cout << endl;
-        //цикл для пробелов
     }
 
+    for (c = 1; c < a; c++)
+        cout << " ";
+    cout << "*";
 }
 
