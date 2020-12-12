e93994df22152ef9ef3e93d15ec83f17ca7e5a19
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2020-12-13 00:28:18 +0200

    added lesson_2.1

---
+++ Lesson_2/Task_1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int shkolu[10];
+    int myschool = 0;
+    for (int i=0; i<10; i++){
+        cout << "vvedi nomer shkoli:";
+        cin >> shkolu[i];
+    }
+    cout << "a teper vvedi svoyu shkolu:";
+    cin >> myschool;
+    for (int i=0; i<10; i++){
+        if (myschool == shkolu[i]){
+            cout << "ya znayu etu shkolu!";
+            break;
+        }
+        if (i + 1 >= 10)
+            break;
+    }
+    return (0);
+}
+
