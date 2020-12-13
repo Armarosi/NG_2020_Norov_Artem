891507d0a15b872927e80b094fdcf9554d57bcc1
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2020-12-13 23:44:40 +0200

    added lesson 2 task 2

---
+++ Lesson_2/Task_2/main.cpp
@@ -0,0 +1,33 @@
+    #include <iostream>
+
+    using namespace std;
+int main()
+{
+    int kartu[10];
+    kartu[0] = 0;
+    kartu[1] = 0;
+    kartu[2] = 0;
+    kartu[3] = 0;
+    kartu[4] = 0;
+    kartu[5] = 0;
+    kartu[6] = 0;
+    kartu[7] = 0;
+    kartu[8] = 0;
+    kartu[9] = 0;
+    int nomer = 0;
+    int money = 0;
+    int allmoney = 0;
+    for ( ; ; ){
+        cout << "vvedi nomer kartochki:";
+        cin >> nomer;
+        cout << "skolko polozhit?";
+        cin >> money;
+        for (int i = 0; i<10; i++){
+            cout << kartu[i] << " ";
+        }
+        kartu[nomer] = kartu[nomer] + money;
+        cout << kartu[nomer] << endl;
+        allmoney = kartu[0] + kartu[1] + kartu[2] + kartu[3] + kartu[4] + kartu[5] + kartu[6] + kartu[7] + kartu[8] + kartu[9];
+        cout << "v summe na vseh kartah " << allmoney << endl;
+    }
+}
