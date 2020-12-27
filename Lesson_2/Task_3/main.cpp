473cffeadcbb945aaefda73976d3e831cd622bf5
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2020-12-27 19:16:57 +0200

    update task3.2

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -21,19 +21,16 @@ int main()
             maxc = chisla[i];
         }
     }
-    int minc = chisla[0];
+    int space = 0;
     for (int i = 0; i<20; i++){
-        if(chisla[i] > minc){
-            minc = chisla[i];
-        }
-    }
-    for (int i = 0; i<20; i++){
-
-        for (int c = 1; c < maxc - chisla[i]  ; c++)
+        space = (maxc - chisla[i])/2;
+        while (space>0){
             cout << " ";
+            space--;
+        }
         for (int n = 0; n < chisla[i]; n++)
             cout << "*";
-
         cout << endl;
     }
 }
+
