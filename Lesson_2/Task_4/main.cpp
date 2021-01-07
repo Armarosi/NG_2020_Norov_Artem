33fc03e27822805f8dee4952665926a6b197b397
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2021-01-07 21:51:20 +0200

    added task_4

---
+++ Lesson_2/Task_4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int column[5];
+    for (int i = 0; i<5; i++){
+        cout << "stars in " << i+1 << " column:";
+        cin >> column[i];
+    }
+
+    int maxc = column[0];
+    for (int i = 0; i<5; i++){
+        if(column[i] > maxc){
+            maxc = column[i];
+        }
+    }
+    for (int i = 0; i < maxc; i++){
+
+        for (int k = 0; k < 5; k++)
+        {
+            if (i < column[k])
+            {
+                cout << "*";
+            }else cout << " ";
+        }
+        cout << endl;
+    }
+}
+
