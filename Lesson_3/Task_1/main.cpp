509f134c1d75b7c40cff074794ec784a8f1b58a1
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2021-01-13 21:01:48 +0200

    Delete 'counter' from Task3.1

--- Lesson_3/Task_1/main.cpp
+++ Lesson_3/Task_1/main.cpp
@@ -5,20 +5,19 @@ using namespace std;
 int main()
 {
     int words = 0;
-    int counter = 0;
     char symbols[100];
     cout << "Enter string: ";
     cin.getline(symbols, 100);
     for(int i = 0; symbols[i] != 0; i++){
             if ((symbols[i] >= 'A' && symbols[i] <= 'Z') || (symbols[i] >= 'a' && symbols[i] <= 'z')){
-                counter++;
-            }
-            if(!((symbols[i] >= 'A' && symbols[i] <= 'Z') || (symbols[i] >= 'a' && symbols[i] <= 'z')) && counter > 0){
                 words++;
-                counter = 0;
+            }
+            if(!((symbols[i] >= 'A' && symbols[i] <= 'Z') || (symbols[i] >= 'a' && symbols[i] <= 'z')) && words > 0){
+                words++;
+                words = 0;
             }
         }
-    if(counter > 0){
+    if(words > 0){
         words++;
     }
     cout << "Result: " << words << endl;
