4b10cdadba5c2f9dc069ff4a047ea997427791c3
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2021-01-18 13:27:01 +0200

    Update Task2.3 by do..while

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -1,29 +1,45 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    int maxValue = 0;
-    int stars[5];
-    int maxV_counter = 0;
+    int chisla[20];
+    for (int i = 0; i<20; i++){
+        chisla[i] = 0;
+    }
+    int i = 0;
+    do{
+        cout << "Enter number:";
+        cin >> chisla[i];
+        if (chisla [i] == 0){
+            break;
+        }
+        i++;
+    }while(i<20);
 
-    for(int counter_input = 0; counter_input < 5; counter_input++){
-        cout << "Enter number in column: ";
-        cin >> stars[counter_input];
-        if (stars[maxV_counter] > maxValue){
-            maxValue = stars[maxV_counter];
+    int maxc = chisla[0];
+    for (int i = 0; i<20; i++){
+        if(chisla[i] > maxc){
+            maxc = chisla[i];
         }
     }
-
-    for(int counter_output = 0; counter_output < maxValue; counter_output++){
-        for(int row = 0; row < 5; row++){
-            if(counter_output < stars[row]){
-                cout << "*";
-            }else{
-                cout << " ";
-            }
+    int minc = chisla[0];
+    int space = 0;
+    for (int i = 0; i<20; i++){
+        if(chisla[i] > minc){
+            minc = chisla[i];
         }
+    }
+    for (int i = 0; i<20; i++){
+
+        for (int c = 1; c < maxc - chisla[i]  ; c++)
+        space = (maxc - chisla[i])/2;
+        while (space>0){
+            cout << " ";
+            space--;
+        }
+        for (int n = 0; n < chisla[i]; n++)
+            cout << "*";
+
         cout << endl;
     }
 }
