22d232ecde88992b1fee92d8e3bdc932d4ad2fb0
Alonzomer <74153402+Alonzomer@users.noreply.github.com>
2021-01-21 22:52:29 +0200

    added Task3.3

--- Lesson_3/Task_3/main.cpp
+++ Lesson_3/Task_3/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    char symbols[100];
+    cout << "Enter string: ";
+    cin.getline(symbols, 100);
+
+    cout << "Result: ";
+    for (int i = 0; i < symbols[i]; i++){
+        if(symbols[i] >= 'a' && symbols[i] <= 'z'){
+            symbols[i] -= 32;
+            cout << symbols[i];
+        }else{
+            cout << symbols[i];
+        }
+    }
 }
